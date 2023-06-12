9c5acc039021a02e114e9bb1c2520868758dedbe
Vladimir <vermakov410@gmail.com>
2023-06-13 01:06:12 +0300

    Задача 3

--- Program.cs
+++ Program.cs
@@ -27,24 +27,44 @@
 // Console.WriteLine("Введите число");
 // string num = Console.ReadLine();
 
-int Num()
-{
-    Console.WriteLine("Введите число");
-    string num = Console.ReadLine();
-    int result = int.Parse(num);
-    return result;
-}
+// int Num()
+// {
+//     Console.WriteLine("Введите число");
+//     string num = Console.ReadLine();
+//     int result = int.Parse(num);
+//     return result;
+// }
 
-int Sum(int number)
+// int Sum(int number)
+// {
+//     int sum = 0;
+//     while(number > 0)
+//     {
+//         sum = sum + (number %10);
+//         number = number / 10;
+//     }
+//     return sum;
+// }
+
+// int number = Num();
+// Console.WriteLine($"Сумма чисел в введённом числе {number} равна {Sum(number)}");
+
+
+
+// Задача 29: Напишите программу, которая 
+// задаёт массив из 8 элементов и выводит 
+// их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+void NewArray()
 {
-    int sum = 0;
-    while(number > 0)
+    int [] array = new int [8];
+    for (int i = 0; i < array.Length; i++)
     {
-        sum = sum + (number %10);
-        number = number / 10;
+        array[i] = new Random().Next(0, 10000);
+        Console.Write($"{array[i]}, ");
     }
-    return sum;
-}
 
-int number = Num();
-Console.WriteLine($"Сумма чисел в введённом числе {number} равна {Sum(number)}");
+}
+NewArray();
